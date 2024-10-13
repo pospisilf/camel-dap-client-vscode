41a113fb62ef905ad6c527710395f3340a80cdad
Filip Pospisil <fpospisi@redhat.com>
2024-10-13 22:00:52 +0200

    fix postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,9 +2,10 @@
 
 const dapServerVersion = '1.4.0-SNAPSHOT';
 
-const download = require('mvn-artifact-download').default;
-const fs = require('fs');
-const path = require('path');
+import pkg from "mvn-artifact-download"; // Default import
+const { default: download } = pkg; // Destructure the download function
+import * as fs from 'fs';
+import * as path from 'path';
 
 const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/snapshots/';
 
