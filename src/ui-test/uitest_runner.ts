0ff333b207d7a8c4d2b92be6a19237e6fa2b1393
Filip Pospisil <fpospisi@redhat.com>
2025-06-11 17:01:45 +0200

    update extester and observe

--- src/ui-test/uitest_runner.ts
+++ src/ui-test/uitest_runner.ts
@@ -14,67 +14,75 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
+"use strict";
 
-import * as path from 'path';
-import * as fs from 'fs';
-import * as variables from './variables';
-import { ExTester, ReleaseQuality } from 'vscode-extension-tester';
+import * as path from "path";
+import * as fs from "fs";
+import * as variables from "./variables";
+import { ExTester, ReleaseQuality } from "vscode-extension-tester";
 
-process.on('unhandledRejection', (reason) => {
-    console.error('Unhandled Rejection:', reason);
+process.on("unhandledRejection", (reason) => {
+  console.error("Unhandled Rejection:", reason);
 });
 
 export const storageFolder = variables.TEST_RESOURCES_DIR;
-const releaseType: ReleaseQuality = process.env.CODE_TYPE === 'insider' ? ReleaseQuality.Insider : ReleaseQuality.Stable;
-export const projectPath = path.resolve(__dirname, '..', '..');
+const releaseType: ReleaseQuality =
+  process.env.CODE_TYPE === "insider"
+    ? ReleaseQuality.Insider
+    : ReleaseQuality.Stable;
+export const projectPath = path.resolve(__dirname, "..", "..");
 const extensionFolder = variables.EXTENSION_DIR;
-const coverage = process.argv[2] === 'coverage';
+const coverage = process.argv[2] === "coverage";
 const deploy = process.argv[2];
 
 async function main(): Promise<void> {
-	const tester = new ExTester(storageFolder, releaseType, extensionFolder, coverage);
+  const tester = new ExTester(
+    storageFolder,
+    releaseType,
+    extensionFolder,
+    coverage
+  );
 
-	let tests: string | string[] = '';
-	let settings: string = './src/ui-test/resources/';
+  let tests: string | string[] = "";
+  let settings: string = "./src/ui-test/resources/";
 
-	switch (deploy) {
-		case 'minikube':
-			tests = 'out/src/ui-test/tests/deploy.kubernetes*.test.js';
-			settings += 'vscode-settings-minikube.json';
-			break;
-		case 'openshift':
-			tests = 'out/src/ui-test/tests/deploy.kubernetes*.test.js';
-			// openshift deployment is tested with default settings deploy parameters
-			settings += 'vscode-settings.json';
-			break;
-		default:
-			tests = [
-				'out/src/ui-test/env/set.camel.version.js',
-				'out/src/ui-test/tests/!(deploy)*.test.js', // run everything, except deployment tests
-				'out/src/ui-test/env/check.camel.version.js'
-			];
-			settings += 'vscode-settings.json';
-			break;
-	}
+  switch (deploy) {
+    case "minikube":
+      tests = "out/src/ui-test/tests/deploy.kubernetes*.test.js";
+      settings += "vscode-settings-minikube.json";
+      break;
+    case "openshift":
+      tests = "out/src/ui-test/tests/deploy.kubernetes*.test.js";
+      // openshift deployment is tested with default settings deploy parameters
+      settings += "vscode-settings.json";
+      break;
+    default:
+      tests = [
+        "out/src/ui-test/env/set.camel.version.js",
+        "out/src/ui-test/tests/01_install.test.js", // run everything, except deployment tests
+        "out/src/ui-test/env/check.camel.version.js",
+      ];
+      settings += "vscode-settings.json";
+      break;
+  }
 
-	await tester.setupAndRunTests(
-		tests,
-		process.env.CODE_VERSION,
-		{
-			'installDependencies': true
-		},
-		{
-			'cleanup': true,
-			'settings': settings,
-			resources: []
-		}
-	);
-	fs.rmSync(extensionFolder, { recursive: true });
+  await tester.setupAndRunTests(
+    tests,
+    process.env.CODE_VERSION,
+    {
+      installDependencies: true,
+    },
+    {
+      cleanup: true,
+      settings: settings,
+      resources: [],
+    }
+  );
+  fs.rmSync(extensionFolder, { recursive: true });
 }
 
 if (require.main === module) {
-	main().catch((error) => {
-		throw Error('Unhandled promise rejection in main: ', error);
-	});
+  main().catch((error) => {
+    throw Error("Unhandled promise rejection in main: ", error);
+  });
 }
